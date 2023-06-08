ad18847c5996637dc8ae76f6df0c3b55b0dd8ef2
SylveonDev <spongey@usa.com>
2023-06-07 21:32:32 -0400

    Sally death animation and audio editing

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -3751,6 +3751,10 @@ class PlayState extends MusicBeatState
 				for (timer in modchartTimers) {
 					timer.active = true;
 				}
+				if (SONG.player1 == "sally"){
+					GameOverSubstate.deathSoundName = 'fnf_loss_sfx-sally';
+					GameOverSubstate.characterName = 'sally';
+				}
 				openSubState(new GameOverSubstate(boyfriend.getScreenPosition().x - boyfriend.positionArray[0], boyfriend.getScreenPosition().y - boyfriend.positionArray[1], camFollowPos.x, camFollowPos.y));
 
 				// MusicBeatState.switchState(new GameOverState(boyfriend.getScreenPosition().x, boyfriend.getScreenPosition().y));
