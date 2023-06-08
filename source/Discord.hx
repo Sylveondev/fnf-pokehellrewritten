f3900a29bf932b3321399ec77a558742e0c937dd
SylveonDev <spongey@usa.com>
2023-06-07 20:31:30 -0400

    A little bit of chart files

--- source/Discord.hx
+++ source/Discord.hx
@@ -18,7 +18,7 @@ class DiscordClient
 		trace("Discord Client starting...");
 		
 		DiscordRpc.start({
-			clientID: "963344162600656947",
+			clientID: "1116151750697689088",
 			onReady: onReady,
 			onError: onError,
 			onDisconnected: onDisconnected
